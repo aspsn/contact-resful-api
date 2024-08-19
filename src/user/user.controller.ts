d7ff133e66b5cec0f8eaea7c23871103cbf1e0b6
Asep <asepsanusisan@gmail.com>
2024-08-19 11:40:02 +0700

    setup test module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { RegisterUserRequest, UserResponse } from '../model/user.model';
 import { WebResponse } from '../model/web.model';
 import { UserService } from './user.service';
@@ -8,6 +8,7 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
+  @HttpCode(200)
   async register(
     @Body() request: RegisterUserRequest,
   ): Promise<WebResponse<UserResponse>> {
--- test/user.spec.ts
+++ test/user.spec.ts
@@ -1,21 +1,34 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import * as request from 'supertest';
+import { Logger } from 'winston';
 import { AppModule } from '../src/app.module';
+import { TestModule } from './test.module';
+import { TestService } from './test.service';
 
 describe('UserController', () => {
   let app: INestApplication;
+  let logger: Logger;
+  let testService: TestService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TestModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    logger = app.get(WINSTON_MODULE_PROVIDER);
+    testService = app.get(TestService);
   });
 
   describe('POST /api/users', () => {
+    beforeEach(async () => {
+      await testService.deleteUser();
+    });
+
     it('should be rejected if request is invalid', async () => {
       const response = await request(app.getHttpServer())
         .post('/api/users')
@@ -24,6 +37,40 @@ describe('UserController', () => {
           password: '',
           name: '',
         });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it('should be able to register', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test',
+          password: 'test',
+          name: 'test',
+        });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.username).toBe('test');
+      expect(response.body.data.name).toBe('test');
+    });
+
+    it('should be rejected if username already exists', async () => {
+      await testService.createUser();
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test',
+          password: 'test',
+          name: 'test',
+        });
+
+      logger.info(response.body);
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toBeDefined();
